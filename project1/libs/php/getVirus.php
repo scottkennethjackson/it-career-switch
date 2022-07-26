4248105b437fb5ff7d235531ae56f5c04ebfadd8
Scott Kenneth Jackson <86068263+scottkennethjackson@users.noreply.github.com>
2022-07-26 14:38:04 +0100

    Update getVirus.php

--- project1/libs/php/getVirus.php
+++ project1/libs/php/getVirus.php
@@ -3,7 +3,7 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, [
-	CURLOPT_URL => "https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php?country=" . $_REQUEST["param1"],
+	CURLOPT_URL => "https://coronavirus-smartable.p.rapidapi.com/stats/v1/" . $_REQUEST["param1"] . "/",
 	CURLOPT_RETURNTRANSFER => true,
 	CURLOPT_FOLLOWLOCATION => true,
 	CURLOPT_ENCODING => "",
@@ -12,7 +12,7 @@ curl_setopt_array($curl, [
 	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 	CURLOPT_CUSTOMREQUEST => "GET",
 	CURLOPT_HTTPHEADER => [
-		"X-RapidAPI-Host: coronavirus-monitor.p.rapidapi.com",
+		"X-RapidAPI-Host: coronavirus-smartable.p.rapidapi.com",
 		"X-RapidAPI-Key: aae7c92896mshe10256639238b66p178ba0jsnf152b11cfc5c"
 	],
 ]);
