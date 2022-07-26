170b9a40b2c76dbb11a0830d6d970c83f800bd21
Scott Kenneth Jackson <86068263+scottkennethjackson@users.noreply.github.com>
2022-07-26 18:29:54 +0100

    Update getPolygon.php

--- project1/libs/php/getPolygon.php
+++ project1/libs/php/getPolygon.php
@@ -1,28 +1,25 @@
 <?php
 
-ini_set("display_errors", "On");
-error_reporting(E_ALL);
-
 $executionStartTime = microtime(true);
 
-$data = file_get_contents("../../countryBorders.geo.json");
-$decode = json_decode($data, true);
+$url="http://api.geonames.org/earthquakesJSON?north=" . $_REQUEST["param1"] . "&south=" . $_REQUEST["param2"] . "&east=" . $_REQUEST["param3"] . "&west=" . $_REQUEST["param4"] . "&username=sk_jackson";
 
-$countryData = array();
-$countryArray = $decode["features"];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,$url);
 
-foreach ($countryArray as $country) {
-    if ($country["properties"]["iso_a2"] == $_REQUEST["param1"]) {
-        $countryData = $country["geometry"]["coordinates"];
-        break;
-    }
-};
+$result=curl_exec($ch);
+
+curl_close($ch);
+
+$decode = json_decode($result, true);
 
 $output["status"]["code"] = "200";
 $output["status"]["name"] = "ok";
 $output["status"]["description"] = "success";
 $output["status"]["returnedIn"] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output["data"] = $countryData;
+$output["data"] = $decode["geonames"];
 
 header("Content-Type: application/json; charset=UTF-8");
 
