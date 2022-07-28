fc502d58451088b18289c02ff686286c86452b7f
Scott Kenneth Jackson <86068263+scottkennethjackson@users.noreply.github.com>
2022-07-28 09:58:55 +0100

    Update getEarthquakes.php

--- project1/libs/php/getEarthquakes.php
+++ project1/libs/php/getEarthquakes.php
@@ -19,7 +19,7 @@ $output["status"]["code"] = "200";
 $output["status"]["name"] = "ok";
 $output["status"]["description"] = "success";
 $output["status"]["returnedIn"] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output["data"] = $decode["geonames"];
+$output["data"] = $decode;
 
 header("Content-Type: application/json; charset=UTF-8");
 
