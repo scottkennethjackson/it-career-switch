e86bd99a78dbd933e4c87d81239443f0fa301a1e
Scott Kenneth Jackson <86068263+scottkennethjackson@users.noreply.github.com>
2022-11-08 11:54:20 +0000

    Update deleteDepartmentByID.php

--- project2/libs/php/deleteDepartmentByID.php
+++ project2/libs/php/deleteDepartmentByID.php
@@ -91,7 +91,7 @@
 		$output['status']['name'] = "ok";
 		$output['status']['description'] = "success";
 		$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-		$output['data'] = ["Department successfully deleted"];
+		$output['data'] = ["The " . $_REQUEST['param2'] . " department has been successfully deleted"];
 		
 		mysqli_close($conn);
 
