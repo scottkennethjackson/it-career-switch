adc4239a6bad50cbb9699f1c8239b1cab037fbbc
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2023-04-03 21:52:42 +0100

    Create App component

--- scran/src/components/App/App.js
+++ scran/src/components/App/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BusinessList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Scott Kenneth Jackson
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>scran</h1>
+        <SearchBar /> 
+        <BusinessList /> 
+      </div>
+    );
+  }
 }
 
 export default App;
