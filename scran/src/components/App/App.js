434f69c4dec157601e5cde4723fbd447bd99d937
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2023-04-04 21:52:55 +0100

    Moved business info to App component

--- scran/src/components/App/App.js
+++ scran/src/components/App/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
 
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'Margin Otto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Bordertown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>scran</h1>
         <SearchBar /> 
-        <BusinessList /> 
+        <BusinessList businesses={businesses}/> 
       </div>
     );
   }
