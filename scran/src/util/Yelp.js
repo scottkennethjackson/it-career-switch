cd7a63f445ab9ddbf0fca33e0f11a85d50b7918e
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2023-04-09 15:44:38 +0100

    Created Yelp.js

---
+++ scran/src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = 'nJsYKf2ucwmGmjUEyqwq9HMQgqo4w6ngNHiJsza4_h6D3ZqNaPEAOISpKRz7hEVtQLCUy_QBKYaUb472zCCHZRW0X_ZyOlIThlSYlcfcYaw_BnxjzMxp3n8C-MkyZHYx';
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(
+            `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&${location}=LOCATION&${sortBy}=SORT_BY`, {
+                headers: {
+                    Authorization: `Bearer ${apiKey}`
+                }
+            }
+        ).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => ({
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                }));
+            }
+        });
+    }
+};
+
+export default Yelp;
