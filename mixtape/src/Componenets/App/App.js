ffa28b4a7e85b085bb82bad4e797e7afb8344dea
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2023-04-10 21:55:26 +0100

    Pass down Search and Render Results lists

--- mixtape/src/Componenets/App/App.js
+++ mixtape/src/Componenets/App/App.js
@@ -5,15 +5,30 @@ import Playlist from '../Playlist/Playlist';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResults: [
+        {
+          name: '',
+          artist: '',
+          album: '',
+          id: ''
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Mixtape</h1>
         <div className="App">
-          {/* Add a SearchBar component */}
+          <SearchBar />
           <div className="App-playlist">
-            {/* Add a SearchResults component */}
-            {/* Add a Playlist component */}
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist />
           </div>
         </div>
       </div>
--- mixtape/src/Componenets/Track/Track.js
+++ mixtape/src/Componenets/Track/Track.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './Track.css';
 
-let isRemoval;
-{/* ^delete once defined */}
+let isRemoval; /* <-- delete once defined */
 
 class Track extends React.Component {
     renderAction() {
@@ -17,10 +16,10 @@ class Track extends React.Component {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3>{/* <- track name will go here -> */}</h3>
-                    <p>{/* <- track artist will go here -> | <- track album will go here -> */}</p>
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
-                <button className="Track-action">{/* <- + or - will go here -> */}</button>
+                <button className="Track-action">{/* <-- + or - will go here --> */}</button>
             </div>
         )
     }
