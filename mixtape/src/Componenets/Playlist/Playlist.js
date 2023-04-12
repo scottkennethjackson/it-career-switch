fb30b3906069ca6a6874a4ac3f2f8619a7a95ae1
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2023-04-12 19:13:35 +0100

    Functionality to change the name of a Playlist added

--- mixtape/src/Componenets/Playlist/Playlist.js
+++ mixtape/src/Componenets/Playlist/Playlist.js
@@ -3,10 +3,22 @@ import TrackList from '../TrackList/TrackList';
 import './Playlist.css';
 
 class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(e) {
+        this.props.onNameChange(e.target.value);
+
+
+    }
+
     render() {
         return (
             <div className="Playlist">
-                <input defaultValue={'New Mixtape'}/>
+                <input defaultValue={'New Mixtape'} onChange={this.handleNameChange} />
                 <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
