94f4a16d9308bfc6c760cfa7526177274d17e2de
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2023-04-16 18:22:35 +0100

    Update Scran

--- scran/src/util/Yelp.js
+++ scran/src/util/Yelp.js
@@ -1,4 +1,6 @@
-const apiKey = 'nJsYKf2ucwmGmjUEyqwq9HMQgqo4w6ngNHiJsza4_h6D3ZqNaPEAOISpKRz7hEVtQLCUy_QBKYaUb472zCCHZRW0X_ZyOlIThlSYlcfcYaw_BnxjzMxp3n8C-MkyZHYx';
+import yelpToken from "./config";
+
+const apiKey = yelpToken;
 
 const Yelp = {
     search(term, location, sortBy) {
